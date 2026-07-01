95665a84f1beae85c5b8d8950f2800b4b27daaa9
Benjamin Marroquin <benjaminenma99@gmail.com>
2026-07-01 17:08:17 -0400

    caracteristica mostrar estado del ascensor

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const $lucesDelCiruclo = document.querySelectorAll('.piso');
-const $botones = document.querySelectorAll('.button_t')
+const $lucesDelCiruclo = document.querySelectorAll( '.piso' );
+const $botones = document.querySelectorAll( '.button_t' )
+const $panelEstado = document.getElementById( 'panel-estado' );
 let contadorDeLuz = 0;
 let enMovimiento = false;
 let listaVisitar = [];
 let indiceactual = 0;
 
-const esperar = (milisegundos) => new Promise(resolve => setTimeout(resolve, milisegundos));
+const esperar = ( milisegundos ) => new Promise( resolve => setTimeout( resolve, milisegundos ) );
 
 const mostrarLuz = async ( actual ) => {
+    
     $lucesDelCiruclo[ actual ].className = 'piso';
 
-   
-
-    if (enMovimiento){
-        listaVisitar.push(actual);
+    if ( enMovimiento ){
+        listaVisitar.push( actual );
         return;
     } 
 
@@ -21,49 +21,51 @@ const mostrarLuz = async ( actual ) => {
     
     if( contadorDeLuz < actual ){
 
+        $panelEstado.textContent = 'Estado: Ascendiendo ⬆️';
+
         console.log( listaVisitar );
 
         while( contadorDeLuz < actual ){
 
-            const luzActual = $lucesDelCiruclo[contadorDeLuz];
+            const luzActual = $lucesDelCiruclo[ contadorDeLuz ];
             
             luzActual.classList.add( luzActual.getAttribute( 'color' ) );
 
-            await esperar(3000);
+            await esperar( 3000 );
 
             contadorDeLuz++;
 
-            luzActual.classList.remove(luzActual.getAttribute('color'));
+            luzActual.classList.remove( luzActual.getAttribute( 'color' ) );
 
         }
+
     }else{
 
         if ( contadorDeLuz > actual ) {
+
+            $panelEstado.textContent = 'Estado: Descendiendo ⬇️';
 
             console.log( listaVisitar );
 
             while( contadorDeLuz > actual ){
 
-                const luzActual = $lucesDelCiruclo[contadorDeLuz];
+                const luzActual = $lucesDelCiruclo[ contadorDeLuz ];
             
                 luzActual.classList.add( luzActual.getAttribute( 'color' ) );
                 
-                await esperar(3000);
+                await esperar( 3000 );
 
                 contadorDeLuz--;
 
-                luzActual.classList.remove(luzActual.getAttribute('color'));
+                luzActual.classList.remove( luzActual.getAttribute( 'color' ) );
 
             }
         }
     }
 
-    const luzDestino = $lucesDelCiruclo[actual];
-    luzDestino.classList.add(luzDestino.getAttribute('color'));
-    await esperar(3000);
-    luzDestino.classList.remove(luzDestino.getAttribute('color'));
-
-    
+    const luzDestino = $lucesDelCiruclo[ actual ];
+    luzDestino.classList.add( luzDestino.getAttribute( 'color' ) );
+    $panelEstado.textContent = 'Estado: Detenido 🛑';
 
     enMovimiento = false
 
