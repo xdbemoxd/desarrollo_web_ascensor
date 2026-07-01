e42eb246efdfe80c136a3c9579953e09e2681543
Benjamin Marroquin <benjaminenma99@gmail.com>
2026-07-01 00:17:41 -0400

    commit inicial y primera version del ascensor

---
+++ script.js
@@ -0,0 +1,60 @@
+const $lucesDelCiruclo = document.querySelectorAll('.piso');
+const $botones = document.querySelectorAll('.button_t')
+let contadorDeLuz = 0;
+
+const esperar = (milisegundos) => new Promise(resolve => setTimeout(resolve, milisegundos));
+
+const mostrarLuz = async ( actual ) => {
+    $lucesDelCiruclo[ actual ].className = 'piso';
+
+    $botones.forEach(btn => btn.disabled = true);
+    
+    if( contadorDeLuz < actual ){
+
+        while( contadorDeLuz < actual ){
+
+            const luzActual = $lucesDelCiruclo[contadorDeLuz];
+            
+            luzActual.classList.add( luzActual.getAttribute( 'color' ) );
+
+            await esperar(3000);
+
+            contadorDeLuz++;
+
+            luzActual.classList.remove(luzActual.getAttribute('color'));
+
+        }
+    }else{
+        if ( contadorDeLuz > actual ) {
+
+            while( contadorDeLuz > actual ){
+
+                const luzActual = $lucesDelCiruclo[contadorDeLuz];
+            
+                luzActual.classList.add( luzActual.getAttribute( 'color' ) );
+                
+                await esperar(3000);
+
+                contadorDeLuz--;
+
+                luzActual.classList.remove(luzActual.getAttribute('color'));
+
+            }
+        }
+    }
+
+    const luzDestino = $lucesDelCiruclo[actual];
+    luzDestino.classList.add(luzDestino.getAttribute('color'));
+    await esperar(3000);
+    luzDestino.classList.remove(luzDestino.getAttribute('color'));
+
+    $botones.forEach(btn => btn.disabled = false);
+   
+}
+
+$botones.forEach((boton, index) => {
+    boton.addEventListener('click', () => {
+        // Al hacer clic, llamamos a la función pasándole el índice (0, 1, 2, 3...)
+        mostrarLuz(index);
+    });
+});
